ed3bed183e467a0511697257d5881ee2b3163f06
Nathan Waters <nathanwaters94@gmail.com>
2022-03-29 17:51:55 -0700

    lab 11 all set up.

--- src/App.js
+++ src/App.js
@@ -7,20 +7,24 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import BestBooks from './BestBooks';
+import Profile from './Profile';
+import LoginForm from './LoginForm';
 
 class App extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      user: null,
+      user: null
     }
   }
 
   loginHandler = (user) => {
     this.setState({
-      user,
+      user: user
     })
+    console.log(this.state.user);
   }
 
   logoutHandler = () => {
@@ -33,15 +37,28 @@ class App extends React.Component {
     return (
       <>
         <Router>
-          <Header user={this.state.user} onLogout={this.logoutHandler} />
+          <Header user={this.state.user} onLogout={this.logoutHandler} onLogin={this.loginHandler} />
           <Switch>
             <Route exact path="/">
               {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+            <BestBooks/>
+
+
             </Route>
             {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+            <Route exact path="/Profile">
+            <Profile 
+              user={this.state.user}
+            />
+            </Route>
+
+            <Route exact path="/LoginForm">
+              <LoginForm onLogin={this.loginHandler}></LoginForm>
+            </Route>
           </Switch>
           <Footer />
         </Router>
+        
       </>
     )
   }
--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,31 +1,53 @@
+import axios from 'axios';
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: null
     }
   }
 
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
+  getBooks = async () => {
+    try {
+      let results = await axios.get(`${process.env.REACT_APP_SERVER}/books`);
+      console.log(results);
+      this.setState({
+        books: results.data
+      })
+      console.log(this.state.books)
+    } catch (error) {
+      console.log("error: ", error)
+    }
+  }
+
+  componentDidMount() {
+    this.getBooks();
+    console.log(this.state.books)
+  };
 
   render() {
-
     /* TODO: render user's books in a Carousel */
+    let bookItems = this.state.books ? this.state.books.map(book => {
+      return (
+        <Carousel.Item key={book._id}>
+          <h3>{book.title}</h3>
+          <p>{book.description}</p>
+        </Carousel.Item>
+      )
 
+    }) : <h3>These are not the Books you are looking for</h3>
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
+        <Carousel>
+          {bookItems}
+        </Carousel>
       </>
     )
   }
 }
-
 export default BestBooks;
