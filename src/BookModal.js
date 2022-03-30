abb6d88d739aa6dff7452ed19b83fc8d66f89540
Nathan Waters <nathanwaters94@gmail.com>
2022-03-30 14:10:33 -0700

    updating one line

--- src/BookModal.js
+++ src/BookModal.js
@@ -14,6 +14,7 @@ export default class BookModal extends Component {
       email: this.props.user.email,
 
     }
+    this.props.hideModal();
     console.log(newBook);
     this.props.bookHandler(newBook);
   }
